99ca1337f471f46738b1b6b7b2c0459efff6737c
andrew <aostroumov@positrace.com>
2015-01-31 18:35:29 +0200

    Store Kilobyte in BigInt

--- db/migrate/20150129224406_create_logs.rb
+++ db/migrate/20150129224406_create_logs.rb
@@ -1,10 +1,10 @@
 class CreateLogs < ActiveRecord::Migration
   def change
     create_table :logs do |t|
-      t.column :mem_total,  'integer unsigned', default: 0
-      t.column :mem_free,   'integer unsigned', default: 0
-      t.column :swap_total, 'integer unsigned', default: 0
-      t.column :swap_free,  'integer unsigned', default: 0
+      t.column :mem_total,  'bigint unsigned', default: 0
+      t.column :mem_free,   'bigint unsigned', default: 0
+      t.column :swap_total, 'bigint unsigned', default: 0
+      t.column :swap_free,  'bigint unsigned', default: 0
       t.float :cpu_load, :cpu_temp, default: 0
 
       t.timestamps null: false
